7500c56b0584743397073c14b584764d54024ee2
Dukeo0 <vaskogabor89@gmail.com>
2021-10-13 16:09:10 +0200

    progik

---
+++ projekt_4_3.py
@@ -0,0 +1,4 @@
+i = 1
+while i <= 16384:
+    print(str(i) +" euro = " + str(i * 1.65) + " dollar")
+    i *= 2
---
+++ projekt_4_4.py
@@ -0,0 +1,11 @@
+j = 1
+szam = 1
+while (szam <= 27):
+    if (j <= 2):
+        print(szam)
+        j += 1
+    else:
+        szam = szam * 3
+        j = 1
+
+
